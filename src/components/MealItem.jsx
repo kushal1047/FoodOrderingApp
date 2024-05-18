42798cf79c5837d18c56e9f229ff2cdec29a3444
Kushal Niraula <kushal463334@gmail.com>
2024-05-18 13:25:37 +1000

    added a new reusable custom Button component.

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import { currencyFormatter } from "../util/formatting";
+import Button from "../UI/Button";
 export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
@@ -6,10 +8,12 @@ export default function MealItem({ meal }) {
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-description">{meal.description}</p>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to cart</button>
+          <Button>Add to cart</Button>
         </p>
       </article>
     </li>
